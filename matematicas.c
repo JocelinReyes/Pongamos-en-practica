65b016103526afef9829e69399df6ca6f6ed9585
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-02-16 18:58:14 -0600

    matematicas.c

---
+++ matematicas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+  int numero= 12;
+  int numero2 = 24;
+
+// vamos a hacer varias operaciones con estos numeros para poner en [prcatica lo visto
+
+printf ("%d", numero+numero2);
+// aqui estamos sumando
+printf ("%d", numero2 - numero);
+//aqui estamos restando
+printf ("%d", numero * numero2);
+// aqui estamos multiplicando
+printf ("%f", numero2 / numero);
+//aqui estamos dividiendo, colocamos el %f porque no sabemos si nos dara un valos entero o un varo flotante.
+//recuerden que podemos hacer cualquier cosa que nos imagenems en la programacion
+//ahorita estamos aprendiendo a manejar el lenguaje de programacion c
+}
