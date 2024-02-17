6125b5f1d7c281838851b8d0947647bb8b7c6e59
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-02-16 19:03:01 -0600

    mensajeSimple.c

---
+++ mensajeSimple.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+int main( ){
+//bueno expliquemos que qqui vamos a imprimir un mensaje simple.
+printf("holaaaaaaa\n");
+//el return es por que no vamos a tener alguna respuesta
+return 0;
+}
