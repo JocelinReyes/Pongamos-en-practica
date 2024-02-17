445cf991f78e2e928a4c5270cddd3d73931f1a4e
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-02-16 19:08:05 -0600

    practica.c

---
+++ practica.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+//declaramos las variables a utilizar
+double d , e , f = 2.33 ;
+int i = 6 ;
+//estamos multiplicando las variables 
+e = f * i ; 
+printf( "Resultado = %f\n", e);
+d = (int) ( f * i ) ; 
+//tenemos un valor entero
+printf( "Resultado = %f\n", d);
+d = (int) f * i ; 
+printf( "Resultado = %f\n", d);
+return 0;
+}
