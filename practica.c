1b668c094d63f3794046187d6ae8188d2725b8d5
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-10-22 19:32:52 -0600

    Actualizar practica.c

--- practica.c
+++ practica.c
@@ -5,7 +5,7 @@ double d , e , f = 2.33 ;
 int i = 6 ;
 //estamos multiplicando las variables 
 e = f * i ; 
-printf( "Resultado = %f\n", e); //mostramos el resultado 
+printf( "Resultado = %f\n", e); //mostramos el resultado que conseguimos realizando el procedimiento 
 d = (int) ( f * i ) ; 
 //tenemos un valor entero
 printf( "Resultado = %f\n", d);
