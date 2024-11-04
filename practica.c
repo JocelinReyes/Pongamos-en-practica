286b6ac1b3c8923d32d34f69f9a9f42d92423788
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-11-03 20:01:09 -0600

    Actualizar practica.c

--- practica.c
+++ practica.c
@@ -3,7 +3,7 @@ int main() {
 //declaramos las variables a utilizar
 double d , e , f = 2.33 ;
 int i = 6 ;
-//estamos multiplicando las variables 
+//estamos multiplicando las variables f e i
 e = f * i ; 
 printf( "Resultado = %f\n", e); //mostramos el resultado que conseguimos realizando el procedimiento 
 d = (int) ( f * i ) ; 
