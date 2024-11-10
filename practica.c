7fe71f01627c93fb813d89efc309edb2453c6dd2
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-11-09 19:14:14 -0600

    Actualizar practica.c

--- practica.c
+++ practica.c
@@ -10,6 +10,6 @@ d = (int) ( f * i ) ;
 //tenemos un valor entero
 printf( "Resultado = %f\n", d);
 d = (int) f * i ; 
-printf( "Resultado = %f\n", d);
+printf( "Resultado = %f\n", d); //imprimimos el resultado 
 return 0;
 }
